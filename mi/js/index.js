12e5c3d6641861f133166e7969acbc74d2e5c8b0
xiebian <864849027@qq.com>
2023-04-08 18:25:49 +0800

    mi

--- mi/js/index.js
+++ mi/js/index.js
@@ -1,1 +1,9 @@
-let pushGoods1 = document.getElementById('pushGoods1')
+let pushGoods1 = document.querySelector('#pushGoods1')
+let navGood1 = document.querySelector('#navGood1')
+
+navGood1.addEventListener('mouseenter', () => {
+  pushGoods1.style.visibility = 'visible'
+})
+navGood1.addEventListener('mouseleave', () => {
+  pushGoods1.style.visibility = 'hidden'
+})
