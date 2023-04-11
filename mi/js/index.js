824adb6b9da4f4e198eb6c54c606000eed107252
xiebian <864849027@qq.com>
2023-04-11 21:20:34 +0800

    mi

--- mi/js/index.js
+++ mi/js/index.js
@@ -1,35 +1,20 @@
-let pushGoods1 = document.querySelector('#pushGoods1')
-let navGood2 = document.querySelector('#navGood2')
-let navGood3 = document.querySelector('#navGood3')
-let navGood4 = document.querySelector('#navGood4')
-let navGood5 = document.querySelector('#navGood5')
-let navGood6 = document.querySelector('#navGood6')
-let navGood7 = document.querySelector('#navGood7')
+const budsTab = document.querySelector('#budsTab')
+const watchsTab = document.querySelector('#watchsTab')
 
-navGood1.addEventListener('mouseenter', visible)
-navGood1.addEventListener('mouseleave', hidden)
+const buds = document.querySelector('#buds')
+const watchs = document.querySelector('#watchs')
 
-navGood2.addEventListener('mouseenter', visible)
-navGood2.addEventListener('mouseleave', hidden)
+budsTab.addEventListener('mouseenter', (e) => {
+  e.target.className = 'active'
+  watchsTab.className = ''
 
-navGood3.addEventListener('mouseenter', visible)
-navGood3.addEventListener('mouseleave', hidden)
+  buds.style.display = 'flex'
+  watchs.style.display = 'none'
+})
+watchsTab.addEventListener('mouseenter', (e) => {
+  e.target.className = 'active'
+  budsTab.className = ''
 
-navGood4.addEventListener('mouseenter', visible)
-navGood4.addEventListener('mouseleave', hidden)
-
-navGood5.addEventListener('mouseenter', visible)
-navGood5.addEventListener('mouseleave', hidden)
-
-navGood6.addEventListener('mouseenter', visible)
-navGood6.addEventListener('mouseleave', hidden)
-
-navGood7.addEventListener('mouseenter', visible)
-navGood7.addEventListener('mouseleave', hidden)
-
-function visible() {
-  pushGoods1.style.visibility = 'visible'
-}
-function hidden() {
-  pushGoods1.style.visibility = 'hidden'
-}
+  buds.style.display = 'none'
+  watchs.style.display = 'flex'
+})
